322c2656d1776a28de77ce629c64f99503a3669b
wollenet <ewollenberg16@jcu.edu>
2018-03-08 13:29:01 -0500

    updating files

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,7 @@ var mongoose = require("mongoose");
 // });
 
 // mLab database
-mongoose.connect("", function(err) {
+mongoose.connect("mongodb://heroku_l6drmvct:58e0a6rgkm90rdse98d9rj5fc6@ds145208.mlab.com:45208/heroku_l6drmvct", function(err) {
 	if(err) throw err;
 	console.log('database connected');
 });
-
-//mongodb://heroku_l6drmvct:58e0a6rgkm90rdse98d9rj5fc6@ds145208.mlab.com:45208/heroku_l6drmvct
